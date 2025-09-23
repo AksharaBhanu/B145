1185daf9e55dbb9fe45e3d67f7333845a824fd15
AksharaBhanu <javagalbhanu@gmail.com>
2025-09-23 17:41:15 +0530

    first commit

---
+++ workspace/B145/day03/demo2.py
@@ -0,0 +1,4 @@
+#comment - this line will not be interpreted by interpretor
+a=10
+print("a") #here it will print a
+print(a)
---
+++ workspace/B145/day03/demo6.py
@@ -0,0 +1,6 @@
+a=10 #initialization
+a=20 #assignment
+a=30 #assignment
+#a variable can hold only one value at a time
+#when we assign new value --> old value will be overwritten by new value
+print(a)
